734a8d464742baf4ae282f2ff0f29a3fbcc1db1d
eradigator <eradigator@gmail.com>
2017-07-28 10:39:25 +0600

    unloader

--- src/main/java/kz/epam/javalab22/entity/Pier.java
+++ src/main/java/kz/epam/javalab22/entity/Pier.java
@@ -1,14 +1,23 @@
 package kz.epam.javalab22.entity;
 
+import java.util.concurrent.Semaphore;
+
 public class Pier {
 
     private int unloadingSpeed;
+    private Semaphore sem;
 
-    public Pier(int unloadingSpeed) {
+    public Pier(int unloadingSpeed, int concurrentThreadsCount) {
         this.unloadingSpeed = unloadingSpeed;
+        this.sem = new Semaphore(concurrentThreadsCount);
+
     }
 
     public int getUnloadingSpeed() {
         return unloadingSpeed;
+    }
+
+    public Semaphore getSem() {
+        return sem;
     }
 }
